6011fa842a3cc7a1054e179ee79567b84316c07d
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-09-15 11:02:11 +0100

    feat: layout

---
+++ js/toggle.js
@@ -0,0 +1,27 @@
+(function () {
+    const toggle = document.querySelector("button[class='toggle']")
+    const pane = document.querySelector(".pane");
+
+    toggle.addEventListener("click", handleToggle)
+
+    pane.addEventListener("click", handleToggle)
+
+    function handleToggle() {
+        const noteList = document.querySelector("aside.notes-list");
+        if (toggle.getAttribute("show") === "false") {
+            noteList.style.display = "flex"
+            noteList.style.animation = "left-right 0.6s ease"
+            pane.style.animation = "bg 0.6s ease"
+            pane.style.display = "block";
+            toggle.setAttribute("show", "true")
+        } else {
+            setTimeout(() => {
+                noteList.style.display = "none";
+                pane.style.display = "none";
+            }, 600)
+            noteList.style.animation = "right-left 0.6s ease"
+            pane.style.animation = "bg-reverse 0.6s ease"
+            toggle.setAttribute("show", "false")
+        }
+    }
+})()
